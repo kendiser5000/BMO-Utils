c779471395fb95054abd17ce45c01482feb0e92b
kendiser5000 <ashraf1sikender@utexas.edu>
2024-10-23 23:48:02 -0700

    Update whisper_transcribe.py

--- voice/whisper_transcribe.py
+++ voice/whisper_transcribe.py
@@ -7,7 +7,7 @@ def process_audio(wav_file, model_name):
     if not os.path.exists(wav_file):
         raise FileNotFoundError(f"WAV file not found: {wav_file}")
 
-    full_command = f"main --file {wav_file} --model /home/pi/git/whisper.cpp/models/ggml-tiny.en.bin -otxt -of speech"
+    full_command = f"main -np --file {wav_file} --model /home/pi/git/whisper.cpp/models/ggml-tiny.en.bin -otxt -of speech"
 
     # Execute the command
     process = subprocess.Popen(full_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
